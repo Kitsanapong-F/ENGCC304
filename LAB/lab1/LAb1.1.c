c98317f50032ccc68a4207d0e964ec2caf6117ef
Kitsanapong-F <09chaisu@gmail.com>
2025-10-04 09:16:27 +0700

    ENG304

--- LAB/lab1/LAb1.1.c
+++ LAB/lab1/LAb1.1.c
@@ -2,5 +2,5 @@
 int main(){
     int num1=0,num2=0,num3=0;
     scanf("%d %d %d",&num1,&num2,&num3);
-    printf("%d",num1+num2+num3);
-}
+    printf("Answer = %d",num1+num2+num3);
+} //end main function
