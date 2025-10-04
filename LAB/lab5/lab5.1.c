63ab066c4fbca62fc966c4694d29377b27147a39
Kitsanapong-F <09chaisu@gmail.com>
2025-10-04 15:13:39 +0700

    ENG

--- LAB/lab5/lab5.1.c
+++ LAB/lab5/lab5.1.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+#include <locale.h>
+
 int main() {
-    char ID[11];  // เก็บรหัสพนักงานได้สูงสุด 10 ตัวอักษร + '\0'
+    char ID[11];
     int hrs = 0, amount = 0;
-    float Salary=0;
+    double Salary = 0;
+
+    // เปิดใช้ locale เพื่อให้ printf รองรับ %' ใส่ comma อัตโนมัติ
+    setlocale(LC_NUMERIC, "");
+
     printf("Input the Employees ID (Max. 10 chars):\n");
-    scanf("%10s", ID);  
+    scanf("%10s", ID);
+
     printf("Input the working hrs:\n");
     scanf("%d", &hrs);
+
     printf("Salary amount/hr:\n");
     scanf("%d", &amount);
+
     Salary = hrs * amount;
-    printf("-----------------------\n");
-    printf("Expected Output:");
-    printf("\nEmployee ID = %s\n", ID);
-    printf("Salary = U$ %.2f",Salary);
+
+    printf("Employees ID = %s\n", ID);
+    printf("Salary = U$ %'.2f\n", Salary);
+
     return 0;
-}//end main function
+}
