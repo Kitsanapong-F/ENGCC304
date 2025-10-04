04b25250426a818aedc4efbc60e9212566f33a96
Kitsanapong-F <09chaisu@gmail.com>
2025-10-04 11:22:23 +0700

    ENG

---
+++ LAB/lab4/Lab4.1.c
@@ -0,0 +1,23 @@
+/*
+    จงเขียนโปรแกรมเพื่อแปลงหน่วยจาก "วัน" เป็น "วินาที"
+    
+    Test case:
+        Input Days : 
+            8
+    Output:
+        8 days = 691200 seconds
+
+    Test case:
+        Input Days : 
+            27
+    Output:
+        27 days = 2332800 seconds
+*/
+#include<stdio.h>
+int main(){
+    int Day=0;
+    printf("Input Days :");
+    scanf("%d",&Day);
+    int seconds=Day*86400 ;
+    printf("%d days = %d  seconds", Day , seconds);
+}//end main function
